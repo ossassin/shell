f6b0a9568c5601c4ba9e42d957ce3317cc436df2
Omer Imran <83036966+ossassin@users.noreply.github.com>
2026-02-11 00:35:40 -0500

    Fixed a bug

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -55,7 +55,7 @@ rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed 
-ls -lR ./data/processed  > ./data/inventory.text
+ls -lR ./data/processed  > ./data/inventory.txt
 
 ###########################################
 
